d20f6176b7ad0a85e6bc863b7a666e7f5c1b69bd
Shehzad Aslam <122732837+Shehzad-Aslam-Ansari@users.noreply.github.com>
2023-09-26 09:26:09 +0000

    updated

--- 19_ArrayLoops/index.js
+++ 19_ArrayLoops/index.js
@@ -23,3 +23,33 @@ for (let item of num){
 for (let i in num){
   console.log(num[i])
 }
+/*
+let num = [3, 5, 1, 2, 4]
+
+// for(let i=0; i<num.length;i++){
+//   console.log(num[i])
+// }
+
+// ForEach Loop
+num.forEach((element) => {
+  console.log(element - element)
+})
+
+// Array.from 
+let name = "Harry"
+let arr = Array.from(name) // Converting in an array
+console.log(arr, typeof(arr))
+
+// for...of 
+  console.log("*****For OF*****")
+for (let item of num){
+  console.log(item)
+}
+
+// for...in
+  console.log("*****For IN*****")
+for (let i in num){
+  console.log(num[i]) //print value
+ // console.log(i) //index of an array
+}
+*/
