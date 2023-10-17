4b1718b4d8cb78dcd5daeed613eacc6844ed0241
Shehzad Aslam <122732837+ShehzadCodeArt@users.noreply.github.com>
2023-10-17 14:30:47 +0000

    practice/revised

---
+++ 00_Practice-Set/00_Self-Pract.js
@@ -0,0 +1,70 @@
+// PS 1
+// Q 1 & 2
+let str = 'Hello';
+let num = 5;
+let mix = str + num;
+
+// console.log(str);
+// console.log(typeof(str));
+// console.log(num);
+// console.log(typeof (num));
+// console.log(mix)
+// console.log(typeof (mix));
+
+// Q 3,4
+const obj = {
+    name: "Bulb",
+    Price: '150'
+}
+// console.log(obj)
+obj["Rate"] = "200"
+// console.log(obj)
+
+// PS2
+// Q1
+// let inp = Number.parseInt((prompt("Enter Num:")));
+// if (inp > 10 && inp < 20) {
+//     console.log(` The number ${inp} Lies between 10 & 20`);
+// }
+// else {
+//     console.log('Number doesn\'t Lies between 10 & 20');
+    
+// }
+
+// Q3
+
+// let divisble = Number.parseInt((prompt("Enter Num:")));
+// if (divisble % 2 == 0 && divisble % 3 == 0) {
+//     console.log(` The number ${divisble} is divisible by both 2  & 3`);
+//     // console.log("bhfbguy");
+// }
+// else {
+//     console.log(`The number ${divisble} doesn\'t divisible by zero`);
+    
+// }
+
+
+// Q4
+
+// let age = Number.parseInt((prompt("Enter Num:")));
+// age > 18 ? console.log("You can drive the car") : console.log("You cannot drive the car");
+
+// if (age > 18 ) {
+//     console.log("You can drive the car");
+//     // console.log("bhfbguy");
+// }
+// else {
+//     console.log("You cannot drive the car");
+    
+// }
+
+// Factorial
+let num1 = 5;
+let fact = 1;
+for (i = 1; i <= num1; i++){
+    fact = fact * i
+}
+console.log(fact);
+
+
+
